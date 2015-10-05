760ab36aae0d6150afa659b92cd0ec5f875f718d
Taejeong Bae <bintriz@gmail.com>
2015-10-05 11:07:55 -0500

    Remove not necessary scripts

--- split_one_sample_from_vcf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-[ $# -lt 2 ] && { echo -e "Usage:\n  $(basename "$0") sample.name input.vcf"; exit 1; }
-
-sample=$1
-file=$2
-
-bcftools view -c1 -Oz -s $sample -o ${file/.vcf*/.$sample.vcf.gz} $file
