788b3c853042c2b04701e4d8f591a1da2a9b4186
Taejeong Bae <bintriz@gmail.com>
2015-11-19 16:08:21 -0600

    Change Exception into FileNotFoundError.

--- utils.py
+++ utils.py
@@ -8,19 +8,28 @@ def check_qdir(subdir=''):
     check_dir(os.path.join('q.out', subdir))
 
 def check_bam_index(bam_file):
-    filename = os.path.splitext(bam_file)[0]
-    if os.path.isfile(filename + '.bai') or os.path.isfile(filename + '.bam.bai'):
+    file_stem = os.path.splitext(bam_file)[0]
+    if os.path.isfile(file_stem + '.bai') or os.path.isfile(file_stem + '.bam.bai'):
         return
     else:
-        msg = "Can't find index for BAM file '{}'".format(bam_file)
-        raise Exception(msg)
+        msg = "Can't find ndex for BAM file '{}'".format(bam_file)
+        raise FileNotFoundError(msg)
 
 def check_ref_index(fasta_file):
     if os.path.isfile(fasta_file + '.fai'):
         return
     else:
         msg = "Can't find index for fasta file '{}'".format(fasta_file)
+        raise FileNotFoundError(msg)
 
+def check_ref_dict(fasta_file):
+    file_stem = os.path.splitext(fasta_file)[0]
+    if os.path.isfile(file_stem + '.dict'):
+        return
+    else:
+        msg = "Can't find dictionary for fasta file '{}'".format(fasta_file)
+        raise FileNotFoundError(msg)
+        
 def parse_sample_list_file(file):
     with open(file) as f:
         return [line.split() for line in f]
@@ -37,3 +46,4 @@ def get_chunk_intervals(fai, chunk_size):
                     end = chrom_size
                 intervals.append((chrom, start, end))
         return intervals
+
