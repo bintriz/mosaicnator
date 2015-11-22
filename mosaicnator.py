848cabfa0878c909ebd04d542cbc9dcdc2fd74b8
Taejeong Bae <bintriz@gmail.com>
2015-11-22 03:56:42 -0600

    Add argument command: somatic_af.

--- mosaicnator.py
+++ mosaicnator.py
@@ -1,21 +1,39 @@
 #!/data2/external_data/Abyzov_Alexej_m124423/apps/pyenv/versions/3.5.0/bin/python
 
 import argparse
-from worker import SomaticCall
-from utils import parse_sample_list_file
+from worker import (
+    SomaticCall,
+    PostProcess,
+    AlleleFreq)
+from utils import (
+    read_sample_pairs,
+    read_samples)
 
 
 def somatic_call(args):
     ref = args.ref
-    sample_list = parse_sample_list_file(args.infile)
     out_dir = 'data.somatic_call'
     
     c = SomaticCall(ref, out_dir)
-    for clone, tissue in sample_list:
+    for clone, tissue in read_sample_pairs(args.infile):
         c.run(clone, tissue)
+    #c.wait()
 
-def somatic_postprocess(args):
-    pass
+def somatic_af(args):
+    ref = args.ref
+    data_dir = 'data.somatic_call'
+    out_dir = 'data.AF'
+
+    print(ref)
+    # p = PostProcess(ref, data_dir)
+    # for sample in read_samples(args.infile):
+    #     p.run(sample)
+    # p.wait()
+    
+    # f = AlleleFreq(ref, data_dir, out_dir)
+    # for clone, tissue in read_sample_pairs(args.infile):
+    #     f.run(clone, tissue)
+    # f.wait()
         
 def main():
     parser = argparse.ArgumentParser(
@@ -32,10 +50,12 @@ def main():
     parser_somatic_call.add_argument(
         'infile',
         metavar='sample_list.txt',
-        help='''Sample list file. 
+        help='''Matched sample list file. 
         Each line format is 
         "clone.bam<tab>tissue.bam" or 
-        "tumor.bam<tab>normal.bam".''')
+        "tumor.bam<tab>normal.bam". 
+        Each column should have full path for bam file.
+        Trailing columns are ignored.''')
     parser_somatic_call.add_argument(
         '--ref',
         metavar='ref.fasta',
@@ -47,6 +67,33 @@ def main():
         help='variant type')
     parser_somatic_call.set_defaults(func=somatic_call)
 
+    # ============================
+    # somatic_af command arguments
+    # ============================
+    
+    parser_somatic_af = subparsers.add_parser(
+        'somatic_af',
+         help='Calculate allele frequency for somatic calls')
+    parser_somatic_af.add_argument(
+        'infile',
+        metavar='sample_list.txt',
+        help='''Matched sample list file. 
+        Each line format is 
+        "clone.bam<tab>tissue.bam" or 
+        "tumor.bam<tab>normal.bam". 
+        Each column should have full path for bam file.
+        Trailing columns are ignored.''')
+    parser_somatic_af.add_argument(
+        '--ref',
+        metavar='ref.fasta',
+        help='refence.fasta file',
+        required=True)
+    parser_somatic_af.add_argument(
+        '--type',
+        choices=['snv', 'indel', 'all'],
+        help='variant type')
+    parser_somatic_af.set_defaults(func=somatic_af)
+    
     # ==================================
     # sensitivity_call command arguments
     # ==================================
