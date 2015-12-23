d6a5bcbf76855019899d1880e1b48842abed4a53
Taejeong Bae <bintriz@gmail.com>
2015-12-23 01:57:44 -0600

    Remove LocalQueue class not in use. Improve print feature.

--- job_queue.py
+++ job_queue.py
@@ -1,14 +1,41 @@
-import multiprocessing as mp
 import subprocess
 import xml.etree.ElementTree as ET
 import time
 
 
 class GridEngineQueue:
-    subq = {}
+    _subq = {}
+    is_1st_print = True
 
     def __init__(self, max_submit=8000):
         self.max_submit = max_submit
+
+    @property
+    def n_submit(self):
+        return len(self.__class__._subq)
+
+    @property
+    def n_submit_in_q(self):
+        return (self._subq_jstate_count('r') +
+                self._subq_jstate_count('qw') +
+                self._subq_jstate_count('hqw'))
+
+    @property
+    def n_done(self):
+        return self._subq_jstate_count('done')
+
+    @property
+    def n_total_in_q(self):
+        return len(self._allq)
+
+    @property
+    def n_run(self):
+        return self._allq_jstate_count('r')
+
+    @property
+    def n_wait(self):
+        return (self._allq_jstate_count('qw') +
+                self._allq_jstate_count('hqw'))
         
     def _update(self):
         xmlstr = subprocess.check_output(['qstat', '-xml']).decode('utf-8')
@@ -21,25 +48,32 @@ class GridEngineQueue:
             jstate = job.find('state').text
             self._allq[jid] = {'name':jname, 'state':jstate}
 
-        for jid in self.__class__.subq:
-            self.__class__.subq[jid]['state'] = self._allq_jstate(jid)
+        for jid in self.__class__._subq:
+            self.__class__._subq[jid]['state'] = self._allq_jstate(jid)
 
-    def _print_jstate(self):
-        n_submit = len(self.__class__.subq)
-        n_q = len(self._allq)
-        n_done = self._subq_jstate_count('done')
-        n_run = self._allq_jstate_count('r')
-        n_wait = self._allq_jstate_count('qw')
-        outstr = '{} submitted: {} done, {} in queue({} in run, {} in wait)'
+    def _print_summary(self):
+        qstat = '  queue status: {:>5} in total  {:>5} in run {:>5} in wait'.format(
+            self.n_total_in_q, self.n_run, self.n_wait)
+        jstat = 'submitted jobs: {:>5} submitted {:>5} done   {:>5} in queue'.format(
+            self.n_submit, self.n_done, self.n_submit_in_q)
+        if self.__class__.is_1st_print == True:
+            self.__class__.is_1st_print = False
+        else:
+            print('\x1b[2A', end='\r')
         print('\x1b[2K', end='\r')
-        print(outstr.format(n_submit, n_done, n_q, n_run, n_wait), end='\r')
+        print('-' * 59)
+        print('\x1b[2K', end='\r')
+        print(qstat)
+        print('\x1b[2K', end='\r')
+        print(jstat, end='\r')
+        
 
     def _allq_wait(self):
         while True:
             self._update()
-            if len(self._allq) < self.max_submit:
+            if self.n_total_in_q < self.max_submit:
                 return
-            self._print_jstate()
+            self._print_summary()
             time.sleep(5)
 
     def _allq_jstate(self, jid):
@@ -53,7 +87,7 @@ class GridEngineQueue:
         return allq_jstate_list.count(jstate)        
 
     def _subq_jstate_count(self, jstate):
-        subq_jstate_list = [jinfo['state'] for jinfo in self.__class__.subq.values()]
+        subq_jstate_list = [jinfo['state'] for jinfo in self.__class__._subq.values()]
         return subq_jstate_list.count(jstate)        
 
     def test_submit(self, q_opt_str, cmd_str):
@@ -66,75 +100,8 @@ class GridEngineQueue:
 
         jid = stdout.split()[2]
         jname = (stdout.split()[3])[2:-2]
-        self.__class__.subq[jid] = {'name':jname}
+        self.__class__._subq[jid] = {'name':jname}
 
         self._update()
-        self._print_jstate()
+        self._print_summary()
         return jid
-
-    def wait(self, job_name=''):
-        while True:
-            self._update()
-            n_done = self._subq_jstate_count('done')
-
-            if  n_done == len(self.__class__.subq):
-                self._print_jstate()
-
-                if job_name != '':
-                    print('\nAll {} jobs done'.format(job_name))
-                else:
-                    print('\nAll jobs done')
-
-                self.__class__.subq = {}
-                return
-
-            self._print_jstate()
-            time.sleep(5)
-
-class LocalQueue:
-    def __init__(self, max_submit=None):
-        cpu_n = mp.cpu_count()
-        if max_submit == None or max_submit > cpu_n:
-            self.max_submit = cpu_n
-        else:
-            self.max_submit = max_submit
-
-        self.q = []
-
-    def _update(self):
-        for p in self.q:
-            if not p.is_alive():
-                self.q.remove(p)
-
-    def _q_wait(self):
-        while True:
-            self._update()
-            if len(self.q) < self.max_submit:
-                return
-            time.sleep(3)
-            
-    def submit(self, cmd_str):
-        self._q_wait()
-        p = mp.Process(
-            target=lambda x: subprocess.run(x, shell=True),
-            args=(cmd_str,))
-        p.start()
-        self.q.append(p)
-
-    def call(self, f):
-        self._q_wait()
-        p= mp.Process(target=f)
-        p.start()
-        self.q.append(p)
-        
-    def wait(self, job_name=''):
-        while True:
-            self._update()
-            if len(self.q) == 0:
-                if job_name != '':
-                    print('All {} jobs done'.format(job_name))
-                else:
-                    print('All jobs done')
-                return
-            time.sleep(3)
-            
