3ad9b0041369e55e74e940949189c7023fe4ba7a
Taejeong Bae <bintriz@gmail.com>
2016-01-12 23:58:11 -0600

    Bug fix: missing \ at a continuing line end.

--- job_scripts/snv_con_call.sh
+++ job_scripts/snv_con_call.sh
@@ -27,13 +27,13 @@ for i in 1 2 3 4; do
     tail -qn+2 $DATADIR/$SAMPLE.{mutect,somaticsniper,strelka,varscan}.snv_AF.txt \
 	|cut -f-5 \
 	|sort -k1,1V -k2,2n -S7G \
-	|uniq -c
+	|uniq -c \
 	|awk -v n=$i '$1 == n {print $2"\t"$3"\t"$4"\t"$5"\t"$6}' >> $CONCALL
 done
 
 CALL_N4=$DATADIR/$SAMPLE.call_n4.snv_AF.txt
 
-printf "#chr\tpos\tref\talt\tsnv_AF\n" > $OUTFILL
+printf "#chr\tpos\tref\talt\tsnv_AF\n" > $OUTFILE
 tail -n+2 $CALL_N4 \
     |awk -v af=$AF '$5 >= af' >> $OUTFILE
 
